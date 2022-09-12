2fcaf6b4d7ef0fff4229890fbc56cb28c47ea5c9
wagnerAJS221 <wagneralex221@gmai.com>
2022-09-12 15:57:38 +0100

    exercise three completed

--- src/numeric-conditions.js
+++ src/numeric-conditions.js
@@ -5,17 +5,39 @@ const ARRAY_TWO = ['Hello', 'Conditions']
 
 // 1. Use conditional statements to set answerOne to false if ARRAY_ONE is not empty
 // or true if it is empty
-let answerOne
+let answerOne 
+
+if (ARRAY_ONE.length == 0){
+  answerOne = true
+} 
+else{
+  answerOne = false
+}
 
 // 2. Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
+if (ARRAY_TWO.length !== 0){
+  answerTwo = true
+}
+else {
+  answerTwo = false
+}
+
 
 const NUM_ONE = 92837
 const NUM_TWO = 92827
 
 // 3. Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
+
+if(NUM_ONE > NUM_TWO){
+  answerThree = true 
+}
+else {
+  answerThree = false
+}
+console.log(answerThree)
 
 const NUMBERS = [67, 2039, 17, 501, 98, 139, 21]
 
