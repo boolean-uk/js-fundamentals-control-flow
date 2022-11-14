6f044e3c7af3606fec43997453e5c79027b8d15e
benji <benjipictor@gmail.com>
2022-11-14 18:37:18 +0000

    all done

--- src/string-conditions.js
+++ src/string-conditions.js
@@ -2,19 +2,25 @@
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
 let answerOne
-
+if (STR_ONE === 'Hello') {
+  answerOne = true
+}
 // Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
 let answerTwo
-
+if (STR_TWO !== STR_ONE) {
+  answerTwo = true
+}
 // Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
 const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
 let answerThree
-
+if (STR_THREE.length > STR_FOUR.length) {
+  answerThree = true
+}
 // Use a combination of a loop and conditional statements to set answerFour
 // to false if STR_FIVE has an odd number of vowels, or true if it has an
 // even number
@@ -34,12 +40,18 @@ let answerFive
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
 let answerSix
+if (STR_SEVEN.length % 2 !== 0) {
+  answerSix = STR_SEVEN[2]
+}
 
 // Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
 let answerSeven
+if (STR_EIGHT.length % 2 === 0) {
+  answerSeven = STR_EIGHT[1] + STR_EIGHT[2]
+}
 
 // Set answerEight to the appropriate season based on what MONTH is set to
 //
@@ -56,6 +68,19 @@ let answerSeven
 const MONTH = 'January'
 
 let answerEight
+if (MONTH === 'January' || MONTH === 'December' || MONTH === 'February') {
+  answerEight = 'Winter'
+} else if (
+  MONTH === 'September' ||
+  MONTH === 'November' ||
+  MONTH === 'October'
+) {
+  answerEight = 'Autumn'
+} else if (MONTH === 'June' || MONTH === 'August' || MONTH === 'July') {
+  answerEight = 'Summer'
+} else if (MONTH === 'March' || MONTH === 'May' || MONTH === 'April') {
+  answerEight = 'Spring'
+}
 
 module.exports = {
   answerOne,
