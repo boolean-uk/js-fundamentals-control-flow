e5cf2e3d516f096862f4fbed288e59da72f2aae7
fatima <fxtimaahmed14@gmail.com>
2023-02-22 11:37:04 +0000

    re-doing boolean conditions

--- src/boolean-conditions.js
+++ src/boolean-conditions.js
@@ -1,5 +1,5 @@
 // Initialise the didPass variable with a boolean value
-let didPass
+let didPass = false
 
 // 1. Create a conditional statement that changes the answer variable to the string
 // "Well done, you passed!" if didPass is true, or "Sorry, try again!" if didPass
@@ -8,6 +8,11 @@ let answer
 
 // 2. When you're done and the test passes, changing didPass to the opposite boolean
 // and run the test again to make sure it still passes
+if (didPass === true) {
+  answer = "Well done, you passed!"
+} else if (didPass === false) {
+  answer = "Sorry, try again!"
+}
 
 // Don't change the code below this line
 module.exports = {
