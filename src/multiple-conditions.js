575401c4ea0a9b962f47c720698b5414105eaf9f
Koci H <husnukoci94@gmail.com>
2023-03-06 21:29:49 +0300

    Finished

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,7 +6,10 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne = true
+let answerOne 
+if (NUM >= LOWER && NUM <= UPPER) {
+  answerOne = true;
+}
 console.log(answerOne)
 
 // Task 2
@@ -17,7 +20,12 @@ const STR = null
 // Set answerTwo to false if it's neither of those
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
-let answerTwo = false
+let answerTwo
+if(STR === 'Hello' || STR === 'Goodbye') {
+  answerTwo = true;
+} else {
+  answerTwo = false;
+}
 console.log(answerTwo)
 
 // Task 3
@@ -36,7 +44,22 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 
-let answerThree = 'Baby'
+let answerThree 
+if((AGE >= 1) && (AGE <= 4)) {
+  answerThree = 'Toddler'
+}
+else if((AGE >= 5) && (AGE <= 12)) {
+  answerThree = 'Child'
+}
+else if((AGE >= 13) && (AGE <= 19)) {
+  answerThree = 'Teenager'
+}
+else if(AGE >= 20) {
+  answerThree = 'Adult'
+} else {
+  answerThree = 'Baby'
+}
+console.log(answerThree)
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
--- src/string-conditions.js
+++ src/string-conditions.js
@@ -1,13 +1,19 @@
 // 1. Use conditional statements to set answerOne to true if STR_ONE is 'Hello'
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
-let answerOne = true
+let answerOne
+if(STR_ONE === 'Hello') {
+  answerOne = true
+}
 console.log(answerOne)
 
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
-let answerTwo = true
+let answerTwo
+if(STR_TWO !== 'Hello') {
+  answerTwo = true
+}
 console.log(answerTwo)
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
@@ -15,7 +21,10 @@ console.log(answerTwo)
 const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
-let answerThree = true
+let answerThree
+if(STR_THREE.length > STR_FOUR.length) {
+  answerThree = true
+}
 console.log(answerThree)
 
 // 4. Use conditional statements to set answerFour to true
@@ -23,7 +32,11 @@ console.log(answerThree)
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
-let answerFour = true
+let answerFour
+if(STR_FIVE.charAt(0).toUpperCase() === STR_FIVE.charAt(STR_FIVE.length - 1).toUpperCase()
+) {
+  answerFour = true
+}
 console.log(answerFour)
 
 // 5. Use conditional statements to set answerFive to true
@@ -31,21 +44,35 @@ console.log(answerFour)
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
-let answerFive = false
+let answerFive
+if (
+  STR_SIX.charAt(0).toUpperCase() ===
+  STR_SIX.charAt(STR_SIX.length - 1).toUpperCase()
+) {
+  answerFive = true 
+} else {
+  answerFive = false
+} 
 console.log(answerFive)
 
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
-let answerSix = 'y'
-console.log(answerSix)
+let answerSix 
+if (STR_SEVEN.length % 2 !== 0) {
+  answerSix = STR_SEVEN.charAt((STR_SEVEN.length - 1) / 2)
+  console.log(answerSix)
+}
 
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
-let answerSeven = 'le'
+let answerSeven
+if(STR_EIGHT.length % 2 === 0) {
+  answerSeven = STR_EIGHT[(STR_EIGHT.length / 2) - 1] + STR_EIGHT[STR_EIGHT.length / 2]
+}
 console.log(answerSeven)
 
 // 8. Set answerEight to the appropriate season based on what MONTH is set to
@@ -60,11 +87,21 @@ console.log(answerSeven)
 // December to February: Winter
 //
 // Run the test after changing the value of MONTH to check you've covered every month correctly
-const MONTH = 'April'
+const MONTH = 'September'
 
-let answerEight = 'Spring'
+let answerEight 
+if(MONTH === 'March' || MONTH === 'April' || MONTH === 'May') {
+  answerEight = 'Spring'
+}
+else if(MONTH === 'June' || MONTH === 'July' || MONTH === 'August') {
+  answerEight = 'Summer'
+}
+else if(MONTH === 'September' || MONTH === 'October' || MONTH === 'November') {
+  answerEight = 'Autumn'
+} else {
+  answerEight = 'Winter'
+}
 console.log(answerEight)
-
 module.exports = {
   answerOne,
   answerTwo,
