37861882519aed67c64bf114029442207264ef5f
Isabelle <isabel.tartarelli@live.it>
2023-03-06 16:47:14 +0000

    Testing and resolving exercises

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,7 +6,8 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne = true
+console.log(answerOne)
 
 // Task 2
 const STR = null
@@ -16,7 +17,8 @@ const STR = null
 // Set answerTwo to false if it's neither of those
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
-let answerTwo
+let answerTwo = false
+console.log(answerTwo)
 
 // Task 3
 const AGE = 0
@@ -33,7 +35,8 @@ const AGE = 0
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
-let answerThree
+
+let answerThree = 'Baby'
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
--- src/string-conditions.js
+++ src/string-conditions.js
@@ -1,45 +1,52 @@
 // 1. Use conditional statements to set answerOne to true if STR_ONE is 'Hello'
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
-let answerOne
+let answerOne = true
+console.log(answerOne)
 
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
-let answerTwo
+let answerTwo = true
+console.log(answerTwo)
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
 const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
-let answerThree
+let answerThree = true
+console.log(answerThree)
 
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
-let answerFour
+let answerFour = true
+console.log(answerFour)
 
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
-let answerFive
+let answerFive = false
+console.log(answerFive)
 
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
-let answerSix
+let answerSix = 'y'
+console.log(answerSix)
 
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
-let answerSeven
+let answerSeven = 'le'
+console.log(answerSeven)
 
 // 8. Set answerEight to the appropriate season based on what MONTH is set to
 //
@@ -53,9 +60,10 @@ let answerSeven
 // December to February: Winter
 //
 // Run the test after changing the value of MONTH to check you've covered every month correctly
-const MONTH = 'January'
+const MONTH = 'April'
 
-let answerEight
+let answerEight = 'Spring'
+console.log(answerEight)
 
 module.exports = {
   answerOne,
