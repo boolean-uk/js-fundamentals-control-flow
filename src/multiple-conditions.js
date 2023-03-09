70200c98f028189debedf71cf1ee6ac59709ea55
Timmyctt <timmyc1990@yahoo.com>
2023-03-09 13:09:40 +0000

    completed core exercises

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -7,7 +7,9 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
 let answerOne
-
+if (NUM >= LOWER && NUM <= UPPER){
+  answerOne = true
+}
 // Task 2
 const STR = null
 
@@ -17,7 +19,12 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
-
+if (STR === 'Hello' || STR === 'Goodbye'){
+  answerTwo = true
+}
+else {
+  answerTwo = false
+}
 // Task 3
 const AGE = 0
 
@@ -34,6 +41,17 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+if (AGE === 0) {
+  answerThree = 'Baby'
+} else if (AGE >= 1 && AGE < 5) {
+  answerThree = 'Toddler'
+} else if (AGE >= 5 && AGE < 13) {
+  answerThree = 'Child'
+} else if (AGE >= 13 && AGE < 20) {
+  answerThree = 'Teenager'
+} else answerThree = 'Adult'
+
+console.log('answer-three', answerThree)
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
--- src/string-conditions.js
+++ src/string-conditions.js
@@ -2,11 +2,16 @@
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
 let answerOne
+if (STR_ONE === 'Hello') {
+  answerOne = true
+}
 
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
-
 let answerTwo
+if (STR_TWO !== 'Hello') {
+  answerTwo = true
+}
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
@@ -14,34 +19,47 @@ const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
 let answerThree
-
+if (STR_THREE.length > STR_FOUR.length) {
+  answerThree = true
+}
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
-
 let answerFour
+if (STR_FIVE[0].toUpperCase() === STR_FIVE[8].toUpperCase()) {
+  answerFour = true
+}
 
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
-
 let answerFive
-
+if (STR_SIX[0].toUpperCase() === STR_SIX[5].toUpperCase()) {
+  answerFive = true
+} else {
+  answerFive = false
+}
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
 let answerSix
-
+if (STR_SEVEN.length % 2 === 1) {
+  answerSix = STR_SEVEN.charAt(2)
+}
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
 let answerSeven
-
-// 8. Set answerEight to the appropriate season based on what MONTH is set to
+if (STR_EIGHT.length % 2 === 0) {
+  const eight = STR_EIGHT.length
+  const charOne = STR_EIGHT.charAt(eight / 2 - 1)
+  const charTwo = STR_EIGHT.charAt(eight / 2)
+  answerSeven = charOne + charTwo
+}
 //
 // For example, if MONTH is 'January', answerEight should be 'Winter'
 //
@@ -56,7 +74,19 @@ let answerSeven
 const MONTH = 'January'
 
 let answerEight
-
+if (MONTH === 'March' || MONTH === 'April' || MONTH === 'May') {
+  answerEight = 'Spring'}
+else if (MONTH === 'June' || MONTH === 'July' || MONTH === 'Augest') {
+  answerEight = 'Summer'
+} else if (
+  MONTH === 'September' ||
+  MONTH === 'October' ||
+  MONTH === 'November'
+) {
+  answerEight = 'Autumn'
+} else if (MONTH === 'December' || MONTH === 'January' || MONTH === 'Febuary') {
+  answerEight = 'Winter'
+}
 module.exports = {
   answerOne,
   answerTwo,
