d1c9cb9fd9af4c8c3c6685c09a9a447adf03805d
Callum Hayden <callumhayden@callums-mbp.home>
2023-09-05 15:51:17 +0100

    boolean questions

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,10 +6,13 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne 
+if (NUM >= LOWER && NUM <= UPPER) {
+  answerOne = true
+}
 
 // Task 2
-const STR = null
+const STR = 'Hello'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
@@ -17,6 +20,12 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
+if (STR === 'Hello' || STR === 'Goodbye'){
+  answerTwo = true
+}else {
+  answerTwo = false
+}
+
 
 // Task 3
 const AGE = 0
