496480605a2a6e04b90182c16c41c466c6d11fc8
llllllll-l <vincent.werelius@gmail.com>
2024-02-15 13:38:14 +0100

    Core and Extensions?

--- src/boolean-conditions.js
+++ src/boolean-conditions.js
@@ -4,7 +4,7 @@ let didPass
 // 1. Create a conditional statement that changes the answer variable to the string
 // "Well done, you passed!" if didPass is true, or "Sorry, try again!" if didPass
 // is false
-let answer
+const answer = didPass ? 'Well done, you passed!' : 'Sorry, try again!'
 
 // 2. When you're done and the test passes, changing didPass to the opposite boolean
 // and run the test again to make sure it still passes
