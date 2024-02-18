d46779a27cc2ef0304ca2b871eeb70d957564450
Kanthee Kosolyuthasarn <Kanthee>
2024-02-18 21:28:02 +0100

    Kanthee Kosolyuthasarn - C#3 - 16.02.24: Core + extension ++

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,17 +6,18 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+// let answerOne = NUM >= LOWER ? true : false
+const answerOne = NUM >= LOWER && NUM <= UPPER
 
 // Task 2
-const STR = null
+const STR = 'Goodbye'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
 // Set answerTwo to false if it's neither of those
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
-let answerTwo
+const answerTwo = STR === 'Goodbye' || STR === 'Hello'
 
 // Task 3
 const AGE = 0
@@ -34,6 +35,18 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+
+if (AGE === 0) {
+  answerThree = 'Baby'
+} else if (AGE >= 1 && AGE <= 4) {
+  answerThree = 'Toddler'
+} else if (AGE >= 5 && AGE <= 12) {
+  answerThree = 'Child'
+} else if (AGE >= 13 && AGE <= 19) {
+  answerThree = 'Teenager'
+} else {
+  answerThree = 'Adult'
+}
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
