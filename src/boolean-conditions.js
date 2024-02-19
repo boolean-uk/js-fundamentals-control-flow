1c01853486f9285756c0e585879672c78c433aed
Periklis <perik76@gmail.com>
2024-02-19 15:16:32 +0200

    Numeric done

--- src/boolean-conditions.js
+++ src/boolean-conditions.js
@@ -1,5 +1,5 @@
 // Initialise the didPass variable with a boolean value
-let didPass = false;
+const didPass = false
 let answer = ''
 // 1. Create a conditional statement that changes the answer variable to the string
 // "Well done, you passed!" if didPass is true, or "Sorry, try again!" if didPass
@@ -9,7 +9,7 @@ if (didPass === true) {
 } else {
   answer = 'Sorry, try again!'
 }
-console.log(answer);
+console.log(answer)
 // 2. When you're done and the test passes, changing didPass to the opposite boolean
 // and run the test again to make sure it still passes
 
