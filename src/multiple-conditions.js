868a20394fb551ec5887e7d2bca4c77b42179b5a
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-02-19 15:30:52 +0100

    core, extensions, advanced

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,10 +6,16 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne  
+if(NUM >= LOWER && NUM <= UPPER)
+{
+  answerOne = true
+}else{
+  answerOne = false
+}
 
 // Task 2
-const STR = null
+const STR = 'Goodbye'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
@@ -17,9 +23,14 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
+if(STR === 'Hello'||'Goodbye'){
+  answerTwo = true
+}else{
+  answerTwo = false
+}
 
 // Task 3
-const AGE = 0
+const AGE = 18
 
 // 3. Use conditional statements to set the answerThree variable below to a
 // string value based on what the AGE variable is set to.
@@ -34,6 +45,22 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+if(AGE === 0)
+{
+  answerThree = 'Baby'
+}
+else if(AGE >= 1 && AGE <= 4){
+  answerThree = 'Toddler'
+}
+else if(AGE >= 5 && AGE <= 12){
+  answerThree = 'Child'
+}
+else if(AGE >= 13 && AGE <= 19 ){
+  answerThree = 'Teenager'
+}
+else if(AGE >= 20){
+  answerThree = 'Adult'
+}
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
--- src/string-conditions.js
+++ src/string-conditions.js
@@ -2,11 +2,17 @@
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
 let answerOne
+if(STR_ONE === 'Hello'){
+  answerOne = true
+}else{answerOne = false}
 
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
 let answerTwo
+if(STR_TWO != 'Hello'){
+  answerTwo = true
+}
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
@@ -15,31 +21,53 @@ const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
 let answerThree
 
+if( STR_THREE.length > STR_FOUR.length){
+  answerThree = true
+}
+
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
 let answerFour
+if(STR_FIVE.startsWith('A') && STR_FIVE.endsWith('a')){
+  answerFour = true
+}
 
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
+let answerFive
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
-
-let answerFive
+if(STR_SIX.startsWith('J') && STR_SIX.endsWith('J')){
+  answerFive = true
+}else{
+  answerFive = false
+}
 
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
 let answerSix
+if (STR_SEVEN.length % 2 !== 0) {
+  const middleIndex = Math.floor(STR_SEVEN.length / 2);
+  answerSix = STR_SEVEN.charAt(middleIndex);
+}
+
 
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
 let answerSeven
+
+if (STR_EIGHT.length % 2 === 0) {
+  const middleIndex = STR_EIGHT.length / 2;
+  answerSeven = STR_EIGHT.slice(middleIndex - 1, middleIndex + 1);
+}
+
 
 // 8. Set answerEight to the appropriate season based on what MONTH is set to
 //
@@ -56,6 +84,17 @@ let answerSeven
 const MONTH = 'January'
 
 let answerEight
+
+if (MONTH === 'March' || MONTH === 'April' || MONTH === 'May') {
+  answerEight = 'Spring';
+} else if (MONTH === 'June' || MONTH === 'July' || MONTH === 'August') {
+  answerEight = 'Summer';
+} else if (MONTH === 'September' || MONTH === 'October' || MONTH === 'November') {
+  answerEight = 'Autumn';
+} else if (MONTH === 'December' || MONTH === 'January' || MONTH === 'February') {
+  answerEight = 'Winter';
+}
+
 
 module.exports = {
   answerOne,
