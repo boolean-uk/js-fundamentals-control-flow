bc5a8ef88596d37d816935831cdd314d3a064265
timsakande <70383887+timsakande@users.noreply.github.com>
2024-02-20 01:03:03 +0000

    Update multiple-conditions.js

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,7 +6,11 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne = true;
+if(NUM >= LOWER && NUM <= UPPER){
+  const answerOne = true;
+  console.log(answerOne);
+}
 
 // Task 2
 const STR = null
@@ -16,8 +20,16 @@ const STR = null
 // Set answerTwo to false if it's neither of those
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
-let answerTwo
+let answerTwo = true; 
 
+if(STR == 'Hello' || 'Goodbye'){
+  const answerTwo = true;
+  console.log(answerTwo);
+}
+else {
+  const answerTwo = false;
+  console.log(answerTwo);
+}
 // Task 3
 const AGE = 0
 
