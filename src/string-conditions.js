df64be5f486a47189a56b1a9aef7e67db4044d5a
timsakande <70383887+timsakande@users.noreply.github.com>
2024-02-20 01:04:06 +0000

    Update string-conditions.js

--- src/string-conditions.js
+++ src/string-conditions.js
@@ -1,39 +1,60 @@
 // 1. Use conditional statements to set answerOne to true if STR_ONE is 'Hello'
+
+let answerOne = true;
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
-
-let answerOne
-
+if(STR_ONE == 'Hello'){
+const answerOne = true;
+console.log(answerOne);
+}
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
-let answerTwo
+let answerTwo = true;
+if(STR_TWO !== 'Hello'){
+  const answerTwo = true;
+  console.log(answerTwo);
+}
 
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
 const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
-let answerThree
+let answerThree = true;
+if(STR_THREE > STR_FOUR.length){
+  const answerThree = true;
+  console.log(answerThree);
+}
 
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
-let answerFour
+let answerFour = true;
+if(STR_FIVE.charAt(0) === STR_SIX.charAt(STR_FIVE.length - 1)){
+
+  const answerFour = true;
+  console.log(answerFour);
+}
 
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
-let answerFive
+let answerFive = true; 
+if(STR_SIX.charAt(0) === strLowercase.charAt(STR_SIX.length - 1)){
 
+  const answerFive = true;
+  console.log(answerFive);
+}
+/*
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
-let answerSix
+let answerSix = 
 
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
@@ -68,3 +89,4 @@ module.exports = {
   MONTH,
   answerEight
 }
+*/
