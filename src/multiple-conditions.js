fa739761ea7619f99d4852c3372db69497ab00f0
noahlenn <noahlenn@hotmail.com>
2024-02-21 15:26:52 +0100

    all tasks done

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -7,9 +7,12 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
 let answerOne
+if ((NUM>=LOWER) && (NUM <= UPPER)) {
+  answerOne = true
+} else answerOne = false
 
 // Task 2
-const STR = null
+let STR = 'Hello'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
@@ -18,8 +21,11 @@ const STR = null
 // to verify your code is correct
 let answerTwo
 
+if ((STR === 'Hello') || (STR === 'Goodbye')) {
+  answerTwo = true
+} else answerTwo = false
 // Task 3
-const AGE = 0
+let AGE = 0
 
 // 3. Use conditional statements to set the answerThree variable below to a
 // string value based on what the AGE variable is set to.
@@ -34,6 +40,12 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+if (AGE === 0) answerThree = 'Baby'
+else if (AGE < 5) answerThree = 'Toddler'
+else if (AGE < 13) answerThree = 'Child'
+else if (AGE < 20) answerThree = 'Teenager'
+else  answerThree = 'Adult'
+
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
